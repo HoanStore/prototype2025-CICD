ca5bc0d54155adfd8b5f15f6307af189382dae60
조근완 <kwjo@mqnic.com>
2025-02-10 15:52:35 +0900

    first commit

---
+++ src/main/java/com/hoan/prototype2025cicd/Prototype2025CicdApplication.java
@@ -0,0 +1,13 @@
+package com.hoan.prototype2025cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prototype2025CicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prototype2025CicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hoan/prototype2025cicd/Prototype2025CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoan.prototype2025cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prototype2025CicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
